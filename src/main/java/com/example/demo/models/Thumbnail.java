6bd40bf59cd9e576e608bef050fcc47ff16db45e
Raures <raures778@gmail.com>
2022-11-13 19:40:43 +0200

    Created thumbnail model

---
+++ src/main/java/com/example/demo/models/Thumbnail.java
@@ -0,0 +1,23 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "thumbnails")
+public class Thumbnail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "image")
+    private String image;
+}
