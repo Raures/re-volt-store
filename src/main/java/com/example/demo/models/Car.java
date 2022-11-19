03fee046f1c9bc40d19ad0f58fb68e504b4c027d
Master <raures778@gmail.com>
2022-11-19 12:55:45 +0200

    JOIN ON c.thumbnails_id = t.id

--- src/main/java/com/example/demo/models/Car.java
+++ src/main/java/com/example/demo/models/Car.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 
@@ -11,7 +13,8 @@ import javax.persistence.*;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "cars")
+@Entity
+@Table(name = "cars")
 public class Car {
 
     @Id
@@ -20,12 +23,22 @@ public class Car {
     private Long id;
     @Column(name = "name")
     private String name;
-    @Column(name = "thumbnail_id")
-    private Integer thumbnail_id;
-    @Column(name = "engine_id")
-    private Integer engine_id;
-    @Column(name = "rating_id")
-    private Integer rating_id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "thumbnail_id", referencedColumnName = "id")
+    @Fetch(FetchMode.JOIN)
+    private Thumbnail thumbnail;
+
+    @OneToOne
+    @JoinColumn(name = "engine_id")
+    @MapsId("id")
+    private Engine engine;
+
+    @OneToOne
+    @JoinColumn(name = "rating_id")
+    @MapsId("id")
+    private Rating rating;
+
     @Column(name = "speed")
     private Integer speed;
     @Column(name = "acc")
