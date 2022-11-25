a8342a7209634b88f2a26c6c99efbeb95fb235af
Raures <raures778@gmail.com>
2022-11-25 21:35:44 +0200

    No information

--- src/main/java/com/example/demo/controllers/StoreController.java
+++ src/main/java/com/example/demo/controllers/StoreController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.models.Car;
 import com.example.demo.repository.CarRepository;
 import com.example.demo.repository.EngineRepository;
+import com.example.demo.repository.RatingRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +15,23 @@ import java.util.List;
 @Controller
 public class StoreController {
     private final CarRepository carRepository;
+    private final EngineRepository engineRepository;
+    private final RatingRepository ratingRepository;
 
-    public StoreController(CarRepository carRepository) {
+    public StoreController(CarRepository carRepository,
+                           EngineRepository engineRepository,
+                           RatingRepository ratingRepository) {
         this.carRepository = carRepository;
+        this.engineRepository = engineRepository;
+        this.ratingRepository = ratingRepository;
     }
 
     @GetMapping("/allCars")
     public String getAllCars(Model model) {
 
         model.addAttribute("cars", carRepository.findAll());
+        model.addAttribute("engines", engineRepository.findAll());
+        model.addAttribute("ratings", ratingRepository.findAll());
 
 //        List<Car> cs = new ArrayList<>(carRepository.findAll());
 //
@@ -37,6 +46,6 @@ public class StoreController {
 //                    + c.getMass() + ";");
 //        }
 
-        return "store";
+        return "store_test";
     }
 }
