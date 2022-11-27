f646cba3948fd0f0dea1382ed79cf8e06cb0f859
Raures <raures778@gmail.com>
2022-11-27 13:41:10 +0200

    Pass more info to View

--- src/main/java/com/example/demo/controllers/StoreController.java
+++ src/main/java/com/example/demo/controllers/StoreController.java
@@ -1,50 +1,38 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Car;
 import com.example.demo.repository.CarRepository;
 import com.example.demo.repository.EngineRepository;
 import com.example.demo.repository.RatingRepository;
+import com.example.demo.services.StoreService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class StoreController {
     private final CarRepository carRepository;
     private final EngineRepository engineRepository;
     private final RatingRepository ratingRepository;
+    private final StoreService storeService;
 
     public StoreController(CarRepository carRepository,
                            EngineRepository engineRepository,
-                           RatingRepository ratingRepository) {
+                           RatingRepository ratingRepository,
+                           StoreService storeService) {
         this.carRepository = carRepository;
         this.engineRepository = engineRepository;
         this.ratingRepository = ratingRepository;
+        this.storeService = storeService;
     }
 
     @GetMapping("/allCars")
     public String getAllCars(Model model) {
 
-        model.addAttribute("cars", carRepository.findAll());
+        model.addAttribute("cars", storeService.getCars());
         model.addAttribute("engines", engineRepository.findAll());
         model.addAttribute("ratings", ratingRepository.findAll());
-
-//        List<Car> cs = new ArrayList<>(carRepository.findAll());
-//
-//        for(Car c: cs) {
-//            System.out.println(c.getId() + ". "
-//                    + c.getName() + " "
-//                    + c.getThumbnail().getImage() + " "
-//                    + c.getEngine().getType() + " "
-//                    + c.getRating().getType() + " spd: "
-//                    + c.getSpeed() + " acc: "
-//                    + c.getAcc() + " mass: "
-//                    + c.getMass() + ";");
-//        }
+        model.addAttribute("enginesCounter", storeService.countEngines());
+        model.addAttribute("ratingsCounter", storeService.countRatings());
 
         return "store_test";
     }
