1b2c37490a2373587376197e438efebc8b453b00
Raures <raures778@gmail.com>
2022-11-27 13:40:48 +0200

    Switched to JpaRepository

--- src/main/java/com/example/demo/repository/EngineRepository.java
+++ src/main/java/com/example/demo/repository/EngineRepository.java
@@ -1,8 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.models.Engine;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EngineRepository extends CrudRepository<Engine, Long> { }
+public interface EngineRepository extends JpaRepository<Engine, Long> { }
