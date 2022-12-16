d67d0202bf80377891ffd0a2018154812fc8c3a0
Raures <raures778@gmail.com>
2022-12-16 22:41:58 +0200

    Minor changes

---
+++ src/main/java/com/example/demo/repository/DifficultyRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.models.Difficulty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DifficultyRepository extends JpaRepository<Difficulty, Long> { }
