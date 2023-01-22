671db4cf3b85c7ae961b50aa09da36aa172a3a1b
Raures <raures778@gmail.com>
2023-01-22 20:49:09 +0200

    Added @RequiredArgsConstructor and "order-by" endpoint

--- src/main/java/com/example/demo/controllers/CarsController.java
+++ src/main/java/com/example/demo/controllers/CarsController.java
@@ -1,26 +1,23 @@
 package com.example.demo.controllers;
 
-import com.example.demo.repository.CarRepository;
 import com.example.demo.repository.EngineRepository;
 import com.example.demo.repository.RatingRepository;
 import com.example.demo.services.CarsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class CarsController {
     private final EngineRepository engineRepository;
     private final RatingRepository ratingRepository;
     private final CarsService carsService;
-
-    public CarsController(EngineRepository engineRepository,
-                          RatingRepository ratingRepository,
-                          CarsService carsService) {
-        this.engineRepository = engineRepository;
-        this.ratingRepository = ratingRepository;
-        this.carsService = carsService;
-    }
 
     @GetMapping("/cars")
     public String getAllCars(Model model) {
@@ -30,6 +27,13 @@ public class CarsController {
         model.addAttribute("enginesCounter", carsService.groupCountEnginesPerType());
         model.addAttribute("ratingsCounter", carsService.groupCountRatingsPerType());
 
-        return "cars_simpleview";
+        return "cars";
+    }
+
+    @PostMapping("/cars/order-by")
+    public String submit(@RequestParam(value = "criteria") String x) {
+        System.out.println("GOT A VALUE - " + x);
+
+        return "cars";
     }
 }
