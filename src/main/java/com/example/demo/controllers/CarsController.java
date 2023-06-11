3b66b3a880a726c9810173fa3a2ca36b7357f30c
Raures <raures778@gmail.com>
2023-06-11 23:36:31 +0300

    Updated attributes

--- src/main/java/com/example/demo/controllers/CarsController.java
+++ src/main/java/com/example/demo/controllers/CarsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequiredArgsConstructor
 public class CarsController {
+
     private final CarsService carsService;
 
     @GetMapping("/cars")
@@ -17,11 +18,9 @@ public class CarsController {
                              @RequestParam(name = "orderBy", required = false, defaultValue = "1") int orderBy,
                              @RequestParam(name = "direction", required = false, defaultValue = "asc") String direction) {
         model.addAttribute("items", carsService.getAllCars(orderBy, direction));
-        model.addAttribute("orderBy", orderBy);
-        model.addAttribute("direction", direction);
         model.addAttribute("info", "Viewing all cars in the store.");
         model.addAttribute("filterWindow", carsService.getFilterWindow());
-        model.addAttribute("orderByOptions", carsService.getOrderByOptions());
+        model.addAttribute("orderBy", carsService.getOrderBy(orderBy, direction));
 
         return "shop";
     }
--- src/main/java/com/example/demo/controllers/TracksController.java
+++ src/main/java/com/example/demo/controllers/TracksController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 
-import com.example.demo.repository.DifficultyRepository;
 import com.example.demo.services.TracksService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,18 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class TracksController {
 
     private final TracksService tracksService;
-    private final DifficultyRepository difficultyRepository;
 
     @GetMapping("/tracks")
     public String getAllTracks(Model model,
                                @RequestParam(name = "orderBy", required = false, defaultValue = "1") int orderBy,
                                @RequestParam(name = "direction", required = false, defaultValue = "asc") String direction) {
-        model.addAttribute("tracks", tracksService.getAllTracks(orderBy, direction));
-        model.addAttribute("orderBy", orderBy);
-        model.addAttribute("direction", direction);
-        model.addAttribute("difficulties", difficultyRepository.findAll());
-        model.addAttribute("difficultiesCounter", tracksService.groupCountDifficultiesByType());
+        model.addAttribute("items", tracksService.getAllTracks(orderBy, direction));
+        model.addAttribute("info", "Viewing all tracks in the store.");
+        model.addAttribute("filterWindow", tracksService.getFilterWindow());
+        model.addAttribute("orderBy", tracksService.getOrderBy(orderBy, direction));
 
-        return "tracks";
+        return "shop";
     }
 }
